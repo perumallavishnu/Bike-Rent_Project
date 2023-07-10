9cf2a4630d6c45084338067bceedd74e450034f3
vishnu vardhan <perumalla.vishnu84@gmail.com>
2023-07-10 11:54:38 +0530

    mobile number added to store in local storage array

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,6 +1,7 @@
 function signUp() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    var mobile = document.getElementById("mobile").value;
 
     // Retrieve stored users from local storage
     var users = JSON.parse(localStorage.getItem("users")) || [];
@@ -8,7 +9,8 @@ function signUp() {
     // Create a new user object
     var user = {
         username: username,
-        password: password
+        password: password,
+        mobile: mobile
     };
 
     // Add the user object to the array
@@ -20,6 +22,7 @@ function signUp() {
     // Clear the input fields
     document.getElementById("username").value = "";
     document.getElementById("password").value = "";
+    document.getElementById("mobile").value = "";
 
     alert("Sign up successful! Please login.");
     window.location.href = 'login.html';
