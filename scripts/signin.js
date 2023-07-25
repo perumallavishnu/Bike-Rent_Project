3d020127974d7d28292e7d2a87e2192fdd643bbb
vishnu vardhan <perumalla.vishnu84@gmail.com>
2023-07-25 18:26:18 +0530

    password validation while signing up

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,8 +1,20 @@
+// Function to validate if the password meets the criteria
+function isValidPassword(password) {
+    // Regex to check if the password contains at least one lowercase letter,
+    // one uppercase letter, one digit, and one special character.
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordPattern.test(password);
+}
+
 function signUp() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var mobile = document.getElementById("mobile").value;
 
+    if (!isValidPassword(password)) {
+        alert('Password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one digit, and one special character.');
+        return;
+    }
     // Retrieve stored users from local storage
     var users = JSON.parse(localStorage.getItem("users")) || [];
 
